ca5d424384de2fc05acd19dc292732df1a2fb508
Hertchel <ahertchel@gmail.com>
2026-05-08 03:00:31 +0800

    a

--- SupplyAPI/settings.py
+++ SupplyAPI/settings.py
@@ -85,9 +85,10 @@ AUTH_USER_MODEL = 'api.CustomUser'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 465))
 #EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
