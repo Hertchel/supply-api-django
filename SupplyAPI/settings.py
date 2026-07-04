1f35355167f954553a3ee573ffaa7c8d553fa65e
Hertchel <ahertchel@gmail.com>
2026-07-05 03:40:42 +0800

    added timeout in the settings.py

--- SupplyAPI/settings.py
+++ SupplyAPI/settings.py
@@ -89,6 +89,7 @@ EMAIL_PORT = int(os.getenv('EMAIL_PORT', 465))
 #EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
+EMAIL_TIMEOUT = 15
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
@@ -204,7 +205,7 @@ else:
                 },
             }
         }
-        print(" Using PostgreSQL with individual settings (Production)")
+        print("F Using PostgreSQL with individual settings (Production)")
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
